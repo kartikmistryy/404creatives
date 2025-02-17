fa9d85f3d8217713063981d14ad8e8beeb64ace4
Kartik Mistry <kartikmistry301@gmail.com>
2025-02-17 19:15:19 +0530

    Debugged vercel analytics import

--- app/page.js
+++ app/page.js
@@ -5,13 +5,13 @@ import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import NewsletterModal from "./components/NewsletterModal";
 import Homepage from "./pages/Homepage";
-import { SpeedInsights } from "@vercel/speed-insights/next"
-  
+import {Analytics } from '@vercel/analytics/react'  
+
 export default function App() {
 
   return (
     <div className="w-full h-full flex flex-col justify-between">
-      <SpeedInsights/>
+      <Analytics/>
       <Navbar/>
       <Homepage/>
       <Footer/>
