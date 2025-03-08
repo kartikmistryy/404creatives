47e28e4b56ad9bfdf8bc7dd7da37fac6129b98d1
Kartik Mistry <kartikmistry301@gmail.com>
2025-03-08 12:12:44 +0530

    Fixed some styling in ComingSoon page

--- app/ComingSoon/page.js
+++ app/ComingSoon/page.js
@@ -10,14 +10,14 @@ const chivo = Chivo({
 
 const ComingSoon = () => {
   return (
-    <div className={`${chivo.className} w-full h-screen min-h-screen bg-[url(/comingSoon.gif)] bg-cover bg-center pt-[10vh] lg:pl-10 p-5 relative`}>
+    <div className={`${chivo.className} w-full h-screen min-h-screen bg-[url(/comingSoon.gif)] bg-cover bg-center pt-[10vh] lg:pl-10 pl-5 relative`}>
         <div className='flex lg:flex-row flex-col justify-between w-full max-w-[1600px] h-full mx-auto'>
             <span className='flex flex-col justify-start items-start py-20 lg:w-2/3 sm:w-[75%] w-full'>
                 <h1 className='lg:text-7xl sm:text-5xl text-3xl  font-semibold lg:leading-[130%] sm:leading-[170%] leading-[180%] tracking-wider pr-10 lg:pr-0'>WE'RE STILL PLUGGING THIS PAGE</h1>
                 <p className='lg:text-xl text-lg w-full'>This page is still under construction! We're plugging it in the meantime, head back to the homepage or checkout our latest projects.</p>
             </span>
 
-            <Image src="/Logo/404.png" width={1000} height={1000} alt='404Logo' className='w-[250px] h-[250px] mt-auto ml-auto'/>
+            <Image src="/Logo/404.png" width={1000} height={1000} alt='404Logo' className='w-[250px] h-[250px] mt-auto absolute bottom-0 right-0'/>
         </div>
     </div>
   )
